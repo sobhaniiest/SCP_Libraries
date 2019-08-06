5f34b82417790c546e151f973504afe541e0023a
sobhaniiest <sobhaniiest@gmail.com>
2019-08-06 06:02:26 +0530

    adding Mmain header file of lib folder

---
+++ lib/SCPService.h
@@ -0,0 +1,12 @@
+#ifndef _SCP_SERVICE_H
+#define _SCP_SERVICE_H
+
+#include <scp-services-headers/CPNewPrinterDialog.h>
+#include <scp-services-headers/ppdcache.h>
+#include <scp-services-headers/asyncconn.h>
+#include <scp-services-headers/newprinter.h>
+#include <scp-services-headers/authinfocache.h>
+#include <scp-services-headers/asyncpk.h>
+#include <scp-services-headers/asyncipp.h>
+
+#endif
